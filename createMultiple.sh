5d64d31c4c5c00a1b4fb09a803510c7fd4bcb350
owolana ayomide <ayomideowolana1@gmail.com>
2022-05-03 02:41:17 +0100

    script to create multiple files

--- createMultiple.sh
+++ createMultiple.sh
@@ -16,8 +16,8 @@ if [[ "$1" == "html" ]]
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <title> Document </title>
+</head>  
 <body>
     
 </body>
